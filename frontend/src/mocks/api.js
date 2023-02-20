7b7564c42517bf5c2577d28a730d6ec72f487de1
Michael Moorman <michael.l.moorman@protonmail.com>
2023-02-20 17:09:40 -0600

    More JSON API mock functionality

--- frontend/src/mocks/api.js
+++ frontend/src/mocks/api.js
@@ -29,6 +29,10 @@ users.set('abraar', {
 });
 
 let sessions = new Map();
+let quotes = new Map();
+quotes.set('michael', {
+
+})
 
 //const valid_token = (username, token) => { return token === 'secrettoken93423'; };
 const valid_token = (username, token) => { return true; };
@@ -65,7 +69,7 @@ const handlers = [
     }),
     rest.get('/api/profile/:username', (req, res, ctx) => {
         const { username } = req.params;
-        const { token } = req.headers.get('Authorization').split(' ')[1];
+        const { token } =  req.headers.get('Authorization').split(' ')[1];
         let result = undefined;
         if (users.has(username) && valid_token(username, token)) {
             result = users.get(username);
@@ -82,6 +86,94 @@ const handlers = [
             );
         }
     }),
+    rest.get('/api/history/:username', (req, res, ctx) => {
+        const { username } = req.params;
+        const { token } =  req.headers.get('Authorization').split(' ')[1];
+        if (!users.has(username) || !valid_token(username, token)) {
+            return res(
+                ctx.status(400),
+                ctx.json({ message: `Invalid credentials`})
+            );
+        }
+        let history = []
+        if (quotes.has(username))
+        {
+            history = quotes.get(username);          
+        }
+        return res(
+            ctx.status(200),
+            ctx.json({ message: `Operation successful`})
+        );
+    }),
+    rest.post('/api/quote', async (req, res, ctx) => {
+        const { username, gallon, date } = await req.json();
+        const { token } =  req.headers.get('Authorization').split(' ')[1];
+        if (!users.has(username) || !valid_token(username, token)) {
+            return res(
+                ctx.status(400),
+                ctx.json({ message: `Invalid credentials`})
+            );
+        }
+        if (!quotes.has(username)) {
+            quotes.set(username, []);
+        }
+        let quotes = quotes.get(username);
+        quotes.set(username, quotes.concat({
+            gallon,
+            date,
+            price: 3.00,
+            due: 7000.00,
+        }));
+        return res(
+            ctx.status(200),
+            ctx.json({ message: `Operation succeeded`})
+        );
+    }),
+    rest.post('/api/register', async (req, res, ctx) => {
+        const { username, email, phone, password } = await req.json();
+        if (users.has(username) || password === undefined || email === undefined)
+        {
+            return res(
+                ctx.status(400),
+                ctx.json({ message: `Registration failed`})
+            );
+        }
+        else {
+            users.set(username, {
+                username,
+                password,
+                email,
+                address1: '',
+                address2: '',
+                city: '',
+                state: '',
+                zip: '',
+                phone
+            });
+            return res(
+                ctx.status(200),
+                ctx.json({ message: `Registration succeeded for ${username}`})
+            );
+        }
+    }),
+    rest.post('/api/logout', async (req, res, ctx) => {
+        const { username } = await req.json();
+        const { token } =  req.headers.get('Authorization').split(' ')[1];
+        if (sessions.has(username) && valid_token(username, token)) {
+            sessions.delete(username);
+            return res(
+                ctx.status(200),
+                ctx.json({ message: `Logout successful for ${username}`})
+            );
+        }
+        else {
+            return res(
+                ctx.status(400),
+                ctx.json({ message: `No session found for ${username} or token invalid`})
+            )
+        }
+    }),
+
     // rest.post('/api/profile', (req, res, ctx) => {
     //     const { username, token, name, email, address1, address2, city, state, zip, phone } = req.json();
     //     let result = { username, token, name, email, address1, address2, city, state, zip, phone };
