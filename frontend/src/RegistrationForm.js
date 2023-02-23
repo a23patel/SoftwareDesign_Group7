35268bb7bfcb217f7e35218e0bd36a6d25fffafa
Dosbol Aliev <50006912+dosbolacademic@users.noreply.github.com>
2023-02-23 00:18:54 -0600

    Adding RegistrationForm Component

---
+++ frontend/src/RegistrationForm.js
@@ -0,0 +1,148 @@
+import { React, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import './styling.css'
+
+const client = axios.create({
+  baseURL: 'api',
+})
+
+const RegistrationForm = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
+  const [number, setNumber] = useState('')
+  const navigate = useNavigate()
+  const token = localStorage.getItem('token')
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const handleNumberChange = (e) => {
+    setNumber(e.target.value)
+  }
+  const handleRegisterSubmit = async () => {
+    await client
+      .post('/register', { username, password, email, number })
+      .then((response) => {})
+      .catch((error) => {
+        const status = error.response.status
+        if (status === 400) {
+          alert('Registration failed!')
+          navigate('/registration')
+        }
+      })
+    await client
+      .post('/login', { username, password })
+      .then((response) => {
+        localStorage.clear()
+        localStorage.setItem('token', response.data.token)
+        localStorage.setItem('username', username)
+      })
+      .catch((error) => {
+        const status = error.response.status
+        if (status === 400) {
+          alert('Login failed!')
+          navigate('/login')
+        }
+      })
+    navigate('/profile/edit')
+  }
+
+  if (token) {
+    navigate('/profile/edit')
+  }
+
+  return (
+    <>
+      <br />
+      <center>
+        <h1 className='h1'>CREATE ACCOUNT</h1>
+      </center>
+      <div className='container'>
+        <label className='label' htmlFor='user'>
+          User Name:{' '}
+        </label>
+        <input
+          type='text'
+          id='user'
+          name='user'
+          placeholder='Enter Username'
+          onChange={handleUsernameChange}
+          required
+        />
+        <br />
+        <label className='label' htmlFor='email'>
+          Email:{' '}
+        </label>
+        <input
+          type='email'
+          id='email'
+          name='email'
+          placeholder='Enter email address'
+          onChange={handleEmailChange}
+          required
+        />
+        <br />
+        <label className='label' htmlFor='Phone Number'>
+          {' '}
+          Phone Number:
+        </label>
+        <input
+          type='tel'
+          id='Phone'
+          name='Phone'
+          placeholder='Enter phone number'
+          minLength='10'
+          title='Must contain exactly 10 digits'
+          onChange={handleNumberChange}
+        />
+        <br />
+        <label className='label' htmlFor='password1'>
+          Password:{' '}
+        </label>
+        <input
+          type='password'
+          id='password1'
+          name='password1'
+          placeholder='Enter a password'
+          pattern='(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}'
+          title='Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters'
+          onChange={handlePasswordChange}
+          required
+        />
+        <br />
+        <label className='label' htmlFor='password2'>
+          Confirm Password:{' '}
+        </label>
+        <input
+          type='password'
+          id='password2'
+          name='password2'
+          placeholder='Re-enter password'
+          required
+        />
+        <button type='button' onClick={handleRegisterSubmit}>
+          REGISTER
+        </button>{' '}
+        <br />
+        <center>
+          <h1 className='heading1'>
+            Already have an account? <a href='/login'> Sign in </a>
+          </h1>
+        </center>
+      </div>
+    </>
+  )
+}
+
+export default RegistrationForm
