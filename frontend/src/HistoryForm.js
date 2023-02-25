7a2257301af85c2e74a0dc9e3e3c8619e29229b7
Abraar Patel <87881362+a23patel@users.noreply.github.com>
2023-02-24 19:11:09 -0600

    Updated HistoryForm.js

--- frontend/src/HistoryForm.js
+++ frontend/src/HistoryForm.js
@@ -8,59 +8,6 @@ const formatter = new Intl.NumberFormat('en-US', {
   currency: 'USD',
 })
 
-// const data = [
-//   {
-//     gallons: 3.7,
-//     address: '7001 Calhoun Rd',
-//     city: 'Houston',
-//     state: 'TX',
-//     zipcode: '77001',
-//     date: '2023-01-01',
-//     price: 5.0,
-//     due: 3.7 * 5.0,
-//   },
-//   {
-//     gallons: 4.1,
-//     address: '302 N Grace St',
-//     city: 'Lombard',
-//     state: 'IL',
-//     zipcode: '60645',
-//     date: '2023-01-02',
-//     price: 6.0,
-//     due: 4.1 * 6.0,
-//   },
-//   {
-//     gallons: 6.2,
-//     address: '7132 Nicolls St',
-//     city: 'Riverside',
-//     state: 'CA',
-//     zipcode: '92509',
-//     date: '2023-01-03',
-//     price: 8.0,
-//     due: 6.2 * 8.0,
-//   },
-//   {
-//     gallons: 7.1,
-//     address: '2300 Perry Blvd',
-//     city: 'Atlanta',
-//     state: 'GA',
-//     zipcode: '30314',
-//     date: '2023-01-04',
-//     price: 4.5,
-//     due: 7.1 * 4.5,
-//   },
-//   {
-//     gallons: 5.6,
-//     address: '11802 N Lane Dr',
-//     city: 'Lakewood',
-//     state: 'OH',
-//     zipcode: '44107',
-//     date: '2023-01-05',
-//     price: 7.2,
-//     due: 5.6 * 7.2,
-//   },
-// ]
-
 const HistoryForm = () => {
   const navigate = useNavigate();
   const [ quotes, setQuotes ] = useState([]);
