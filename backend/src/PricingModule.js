7bdddc5aa498fda62b70820711159a15c4f76294
Dosbol Aliev <50006912+dosbolacademic@users.noreply.github.com>
2023-03-18 16:45:14 -0500

    Adding If Statement for This File

--- backend/src/PricingModule.js
+++ backend/src/PricingModule.js
@@ -18,12 +18,11 @@ class FuelDelivery {
   }
 
   getTotalAmountDue() {
+    if (this.gallonsRequested < 0 || this.suggestedPricePerGallon < 0) {
+      return NaN;
+    }
     return this.gallonsRequested * this.suggestedPricePerGallon;
   }
 }
 
 module.exports = FuelDelivery;
-// export default FuelDelivery
-// const delivery = new FuelDelivery(100, '2023-03-08', '123 Main St', 'Anytown', 'CA', '12345', 2.50);
-// const totalAmountDue = delivery.getTotalAmountDue();
-// console.log(totalAmountDue);
