c2386bef63164854728cc12ab1c2f9aa4ecb3910
Michael Moorman <michael.l.moorman@protonmail.com>
2023-03-19 12:27:17 -0500

    Example of profile GET handler

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { generateFuelQuote } = require('./fuelquotes');
+const { generateProfile } = require('./profile');
 
 const PORT = process.env.PORT || 3001;
 
@@ -6,4 +8,16 @@ const app = express();
 
 app.listen(PORT, () => {
     console.log(`Starting server listening on port ${PORT}`);
+});
+
+app.get('/api/profile/:username', (req, res) => {
+    const { username } = req.params;
+    const token = req.headers['Authorization'].split(' ')[1];
+    if (!validate_token(username, token)) {
+        res.status(400).json({ msg: 'Error: invalid login'});
+    } else {
+        // we would use getProfile to get the profile
+        const profile = generateProfile(username);
+        res.status(200).json(profile);
+    }
 });
