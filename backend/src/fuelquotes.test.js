10f4d5486b1bf40638bf8118ecb31208253fda08
Rishi Chitturi <rkchittu@cougarnet.uh.edu>
2023-03-18 23:19:51 -0500

    Minor change to fuelquotes.test.js

--- backend/src/fuelquotes.test.js
+++ backend/src/fuelquotes.test.js
@@ -1,7 +1,7 @@
 const { generateFuelQuote, getQuoteHistory } = require('./fuelquotes')
 
 // Mocking profile data
-const profiledata = {
+const profile = {
   username: 'dosbol',
   address: '123 Hornwood Dr',
   city: 'Houston',
