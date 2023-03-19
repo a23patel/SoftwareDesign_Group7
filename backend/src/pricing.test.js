f56ad39ed82340208a474a1ea634891b66540074
Dosbol Aliev <50006912+dosbolacademic@users.noreply.github.com>
2023-03-18 23:30:31 -0500

    Minor Changes

--- backend/src/pricing.test.js
+++ backend/src/pricing.test.js
@@ -1,6 +1,6 @@
 const FuelDelivery = require('./pricing');
 
-describe('FuelDelivery', () => {
+describe('Princing Module', () => {
   test('getTotalAmountDue() returns the correct amount', () => {
     const delivery = new FuelDelivery(100, '2023-03-08', '123 Main St', 'Anytown', 'CA', '12345', 2.50);
     expect(delivery.getTotalAmountDue()).toBe(250);
