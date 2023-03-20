daacbeb9cc5a181b7135d2cfe75082f1695d9b21
a23patel <abraarpatel23@gmail.com>
2023-03-20 00:36:06 -0500

    Adding GET and POST handlers for profile module

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,23 +1,35 @@
-const express = require('express');
-const { generateFuelQuote } = require('./fuelquotes');
-const { generateProfile } = require('./profile');
+const express = require('express')
+const { generateFuelQuote } = require('./fuelquotes')
+const { generateProfile, updateProfile } = require('./profile')
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
-const app = express();
+const app = express()
 
 app.listen(PORT, () => {
-    console.log(`Starting server listening on port ${PORT}`);
-});
+  console.log(`Starting server listening on port ${PORT}`)
+})
 
 app.get('/api/profile/:username', (req, res) => {
-    const { username } = req.params;
-    const token = req.headers['Authorization'].split(' ')[1];
-    if (!validate_token(username, token)) {
-        res.status(400).json({ msg: 'Error: invalid login'});
-    } else {
-        // we would use getProfile to get the profile
-        const profile = generateProfile(username);
-        res.status(200).json(profile);
-    }
-});
+  const { username } = req.params
+  const token = req.headers['Authorization'].split(' ')[1]
+  if (!validate_token(username, token)) {
+    res.status(400).json({ msg: 'Error: Invalid login' })
+  } else {
+    // we would use generateProfile to get the profile
+    const profile = generateProfile(username)
+    res.status(200).json(profile)
+  }
+})
+
+app.post('/api/profile/edit', (req, res) => {
+  const { username, profileData } = req.params
+  const token = req.headers['Authorization'].split(' ')[1]
+  if (!validate_token(username, token)) {
+    res.status(400).json({ msg: 'Error: Invalid login' })
+  } else {
+    // we would use updateProfile to edit the profile
+    const updatedProfile = updateProfile(username, profileData)
+    res.status(200).json(updatedProfile)
+  }
+})
