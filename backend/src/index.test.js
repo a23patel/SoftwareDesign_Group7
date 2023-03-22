046efa3b7e1b8cfde7aeaf852dfafb92c1f99061
Michael Moorman <michael.l.moorman@protonmail.com>
2023-03-21 22:24:06 -0500

    Adding final bit of test coverage, fixing errors in API documentation

--- backend/src/index.test.js
+++ backend/src/index.test.js
@@ -199,9 +199,151 @@ describe('The Express app', () => {
             .catch(e => { throw e });
     });
     test.skip('should allow users to submit quotes', async () => {
-
+        const username = 'robert';
+        const password = 'testpass2';
+        let reqBody = { username, password };
+        await client.post('/api/register', reqBody).then((response) => {
+            expect(response.data.msg).toBe('Success');
+        }).catch(e => {
+            console.log(`Error: failed to register`);
+            throw e;
+        });
+        let token = undefined;
+        await client.post('/api/login', reqBody).then((response) => {
+            token = response.data.token;
+            expect(token).not.toBeUndefined();
+        }).catch(e => { 
+            console.log(`Error: failed to login`);
+            throw e;
+        });
+        let profileBody = {
+            username,
+            fullname: 'Robert Robertson',
+            email: 'rob@uh.edu',
+            address1: '111 Somewhere Street',
+            address2: 'Apartment 2',
+            city: 'Houston',
+            state: 'TX',
+            phone: '7137137133',
+            zipcode: '77001'
+        };
+        await clientWithAuth(token).post('/api/profile/edit', profileBody)
+            .then((response) => { })
+            .catch(e => {
+                console.log(`Error: failed to modify profile`);
+                throw e;
+            });
+        await clientWithAuth(token).get('/api/profile/' + username)
+            .then((response) => {
+                const { fullname, email, address1, address2, city, state, zipcode, phone } = response.data;
+                expect(fullname).toBe('Robert Robertson');
+                expect(email).toBe('rob@uh.edu');
+                expect(address1).toBe('111 Somewhere Street');
+                expect(address2).toBe('Apartment 2');
+                expect(city).toBe('Houston');
+                expect(state).toBe('TX');
+                expect(zipcode).toBe('7137137133');
+                expect(phone).toBe('77001');
+            }).catch(e => {
+                console.log(`Error: failed to fetch profile`);
+                throw e;
+        });
+        const gallons = 5;
+        let price, due;
+        const date = '2023-04-03';
+        await clientWithAuth(token).get('/api/quote/'+ username + '/' + gallons)
+            .then((response) => {
+                price, due = req.body;
+                expect(price).toBe(1.5);
+                expect(due).toBeCloseTo(gallons*price);
+            }).catch(e => { throw e });
+        const quoteBody = { username, gallons, date, price, due };
+        await clientWithAuth(token).post('/api/quote', quoteBody)
+            .then((response) => { })
+            .catch(e => { throw e });
+        await clientWithAuth(token).post('/api/logout', { username, token })
+            .then((response) => {})
+            .catch(e => { throw e });
     });
     test.skip('should allow users to view quote history', async () => {
-
+        const username = 'robert';
+        const password = 'testpass2';
+        let reqBody = { username, password };
+        await client.post('/api/register', reqBody).then((response) => {
+            expect(response.data.msg).toBe('Success');
+        }).catch(e => {
+            console.log(`Error: failed to register`);
+            throw e;
+        });
+        let token = undefined;
+        await client.post('/api/login', reqBody).then((response) => {
+            token = response.data.token;
+            expect(token).not.toBeUndefined();
+        }).catch(e => { 
+            console.log(`Error: failed to login`);
+            throw e;
+        });
+        let profileBody = {
+            username,
+            fullname: 'Robert Robertson',
+            email: 'rob@uh.edu',
+            address1: '111 Somewhere Street',
+            address2: 'Apartment 2',
+            city: 'Houston',
+            state: 'TX',
+            phone: '7137137133',
+            zipcode: '77001'
+        };
+        await clientWithAuth(token).post('/api/profile/edit', profileBody)
+            .then((response) => { })
+            .catch(e => {
+                console.log(`Error: failed to modify profile`);
+                throw e;
+            });
+        let profile;
+        await clientWithAuth(token).get('/api/profile/' + username)
+            .then((response) => {
+                const { fullname, email, address1, address2, city, state, zipcode, phone } = response.data;
+                profile = response.data;
+                expect(fullname).toBe('Robert Robertson');
+                expect(email).toBe('rob@uh.edu');
+                expect(address1).toBe('111 Somewhere Street');
+                expect(address2).toBe('Apartment 2');
+                expect(city).toBe('Houston');
+                expect(state).toBe('TX');
+                expect(zipcode).toBe('7137137133');
+                expect(phone).toBe('77001');
+            }).catch(e => {
+                console.log(`Error: failed to fetch profile`);
+                throw e;
+        });
+        const gallons = 5;
+        let price, due;
+        const date = '2023-04-03';
+        await clientWithAuth(token).get('/api/quote/'+ username + '/' + gallons)
+            .then((response) => {
+                price, due = req.body;
+                expect(price).toBe(1.5);
+                expect(due).toBeCloseTo(gallons*price);
+            }).catch(e => { throw e });
+        const quoteBody = { username, gallons, date, price, due };
+        await clientWithAuth(token).post('/api/quote', quoteBody)
+            .then((response) => { })
+            .catch(e => { throw e });
+        await clientWithAuth(token).get('/api/history/' + username)
+            .then((response) => {
+                const { gallons, address, city, state, zipcode, date, price, due } = response.data.quotes[0];
+                expect(gallons).toBe(quoteBody.gallons);
+                expect(date).toBe(quoteBody.date);
+                expect(price).toBe(quoteBody.price);
+                expect(due).toBe(quoteBody.due);
+                expect(address).toBe(profile.address1 + profile.address2);
+                expect(city).toBe(profile.city);
+                expect(state).toBe(profile.state);
+                expect(zipcode).toBe(profile.zipcode);
+            }).catch(e => { throw e });
+        await clientWithAuth(token).post('/api/logout', { username, token })
+            .then((response) => {})
+            .catch(e => { throw e });
     });
 });
